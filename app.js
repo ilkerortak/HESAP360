98aab4f0c747254b71f5b66d89cee4c3c1fc5bcb
ilkerortak <ilkerortakofficial@gmail.com>
2026-03-25 06:36:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => { res.render('index'); });
+app.get('/kredi', (req, res) => { res.render('kredi'); });
 app.get('/kredi-limit', (req, res) => { res.render('kredi-limit'); });
 
 const PORT = process.env.PORT || 3000;
