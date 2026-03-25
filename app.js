7a96f2802a9a3bb22c4400bedab12224199d0db0
ilkerortak <ilkerortakofficial@gmail.com>
2026-03-25 06:48:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rotalar
+// Rotalar - HangiKredi & HesapKurdu Modülleri
 app.get('/', (req, res) => res.render('index'));
 app.get('/kredi', (req, res) => res.render('kredi'));
 app.get('/kredi-limit', (req, res) => res.render('kredi-limit'));
 app.get('/mevduat', (req, res) => res.render('mevduat'));
+app.get('/kredi-karti', (req, res) => res.render('kredi-karti'));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Hesap360 Aktif: ${PORT}`));
+app.listen(PORT, () => console.log(`Hesap360 Master Engine Aktif: ${PORT}`));
