dc7a8d00981f943232c395a79cc334253a120ddd
ilkerortak <ilkerortakofficial@gmail.com>
2026-03-25 07:52:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
 
+// Görünüm Motoru Ayarları
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(expressLayouts);
+app.set('layout', 'layout');
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rotalar
-app.get('/', (req, res) => res.render('index'));
-app.get('/arac-maliyet', (req, res) => res.render('arac-maliyet'));
-app.get('/yatirim-hesapla', (req, res) => res.render('yatirim-hesapla'));
+// Ana Sayfa Rotası
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
-app.listen(3000, () => console.log('BiMaliyet v1.0 Yayında!'));
+// Araç Maliyet Sayfası
+app.get('/arac-maliyet', (req, res) => {
+    res.render('arac-maliyet');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Bi'Maliyet Yayında: http://localhost:${PORT}`));
